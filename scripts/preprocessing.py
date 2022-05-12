5674a2be8a85e1b7d85e1185bd3e306d87bf46fc
Yididya <yidisam18@gmail.com>
2022-05-12 10:07:34 +0300

    unit test preprocessing module

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -2,7 +2,10 @@ import sys
 
 import numpy as np
 import pandas as pd
-from logger import Logger
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join('./')))
+# from logger import Logger
 
 
 class PreProcess:
@@ -14,12 +17,12 @@ class PreProcess:
         """
         try:
             self.df = df
-            self.logger = Logger("preprocessing.log").get_app_logger()
-            self.logger.info(
-                'Successfully Instantiated Outlier Class Object')
+            # self.logger = Logger("preprocessing.log").get_app_logger()
+            # self.logger.info(
+            # 'Successfully Instantiated Outlier Class Object')
         except Exception:
-            self.logger.exception(
-                'Failed to Instantiate Preprocessing Class Object')
+            # self.logger.exception(
+            # 'Failed to Instantiate Preprocessing Class Object')
             sys.exit(1)
 
     def convert_to_datetime(self, df, column: str):
@@ -31,8 +34,8 @@ class PreProcess:
         """
         df[column] = pd.to_datetime(
             df[column], errors='coerce')
-        self.logger.info(
-            'Converted datetime columns to datetime')
+        # self.logger.info(
+        # 'Converted datetime columns to datetime')
         return df
 
     def convert_to_float(self, df, column: str):
@@ -43,8 +46,8 @@ class PreProcess:
             column (str): Column to be converted to string
         """
         self.df[column] = df[column].astype(float)
-        self.logger.info(
-            'Successfully converted to float columns')
+        # self.logger.info(
+        # 'Successfully converted to float columns')
         return self.df
 
     def drop_variables(self, df):
@@ -58,8 +61,8 @@ class PreProcess:
         df = df[df.columns[df.isnull().mean() < 0.3]]
         missing_cols = df.columns[df.isnull().mean() > 0]
         print(missing_cols)
-        self.logger.info(
-            'Missing columns are: ', missing_cols)
+        # self.logger.info(
+        #     'Missing columns are: ', missing_cols)
         return df, df_before_filling, missing_cols
 
     def clean_feature_name(self, df):
@@ -70,8 +73,8 @@ class PreProcess:
         """
         df.columns = [column.replace(' ', '_').lower()
                       for column in df.columns]
-        self.logger.info(
-            'Cleaned feature names')
+        # self.logger.info(
+        #     'Cleaned feature names')
         return df
 
     def rename_columns(self, df: pd.DataFrame, column: str, new_column: str):
@@ -82,8 +85,9 @@ class PreProcess:
             column (str): column to be renamed
             new_column (str): New column name
         """
-        df = df[column].rename(new_column)
-        return df
+        df[column] = df[column].rename(new_column)
+        dfRenamed = df.rename({column: new_column}, axis=1)
+        return dfRenamed
 
     def fill_numerical_variables(self, df):
         """Fill numerical variables.
@@ -98,8 +102,8 @@ class PreProcess:
             df_single.loc[:, num_cols].median())
         print(num_cols)
         print(df_single.loc[:, num_cols].median())
-        self.logger.info(
-            'Filled missing numerical variables')
+        # self.logger.info(
+        #     'Filled missing numerical variables')
         return cols, df_single, num_cols
 
     def fill_categorical_variables(self, df, cols, num_cols, df_single):
@@ -117,8 +121,8 @@ class PreProcess:
         df_cols = df_single.columns
         print(cat_cols)
         print(df_single.loc[:, cat_cols].mode().iloc[0])
-        self.logger.info(
-            'Filled missing categorical variables with mode')
+        # self.logger.info(
+        #     'Filled missing categorical variables with mode')
         return df_cols, df_single, cat_cols
 
     def drop_duplicates(self, df):
@@ -139,6 +143,6 @@ class PreProcess:
         """
         megabyte = 1*10e+5
         megabyte_col = df[col] / megabyte
-        self.logger.info(
-            'Converted bytes to megabytes')
+        # self.logger.info(
+        #     'Converted bytes to megabytes')
         return megabyte_col
